1fdd1eb226a7dfb46515dc1899fdae0af564f8b3
xjdxc <290454691@qq.com>
2019-06-22 20:37:04 +0800

    zjg first

---
+++ src/views/BottomBar.vue
@@ -0,0 +1,21 @@
+<template>
+   <div>
+
+
+
+       <bottomBar></bottomBar>
+   </div>
+</template>
+
+<script>
+import bottomBar from '@/components/BottomBar.vue'
+    export default {
+      components:{
+          bottomBar
+      }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
