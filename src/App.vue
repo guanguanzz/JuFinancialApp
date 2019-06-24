bb407201d66300fe2fe43cdfbaa6226db958bf32
xjdxc <290454691@qq.com>
2019-06-24 09:19:59 +0800

    插件配置

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,9 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  background-color: #eee;
+  min-height: 100vh;
 }
 #nav {
   padding: 30px;
--- src/views/BottomBar.vue
+++ src/views/BottomBar.vue
@@ -1,21 +1,24 @@
 <template>
-   <div>
-
-
-
-       <bottomBar></bottomBar>
-   </div>
+    <div>
+        <div class="content">
+            <router-view></router-view>
+        </div>
+        <bottomBar></bottomBar>
+    </div>
 </template>
 
 <script>
-import bottomBar from '@/components/BottomBar.vue'
+    import bottomBar from '@/components/BottomBar.vue'
     export default {
-      components:{
-          bottomBar
-      }
+        components: {
+            bottomBar
+        },
+
     }
 </script>
 
 <style lang="scss" scoped>
-
+ .content{
+     height: calc(100% - 50px); 
+ }
 </style>
